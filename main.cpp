5ac4a4705ff4acdf920ed0f4450889a714372bcc
krr-hws <kramer@hw-schule.de>
2023-03-09 19:30:29 +0000

    Set first string to "Hallo"

---
+++ main.cpp
@@ -0,0 +1,53 @@
+#include "mbed.h"
+#include "i2c-lcd.h"
+#include <string.h>
+using namespace std;
+
+int row=0;
+int col=0;
+
+char *test = "Hallo";
+
+//string testString;
+
+// Für LCD auf Basis-Board:
+// Brücke von D15(SCL) -> PA_11
+// Brücke von D14 (SDA) -> PA_12
+
+ 
+int main() {
+    lcd_init ();
+    lcd_clear ();
+
+    lcd_put_cur(0, 0);
+
+    lcd_send_string ("Hallo");
+
+    ThisThread::sleep_for(2000ms);
+
+    lcd_put_cur(1, 0);
+
+    lcd_send_string("Welt");
+
+    ThisThread::sleep_for(2000ms);
+
+    lcd_clear();
+
+    while (1)
+    {
+
+	    for (int i=0;i<128;i++)
+	    {
+		    lcd_put_cur(row, col);
+
+            lcd_send_data(i+48);
+
+            col++;
+
+            if (col > 15) {row++; col = 0;}
+            if (row > 1) row=0;
+
+            ThisThread::sleep_for(250ms);
+	  }
+  }
+}
